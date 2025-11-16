b4bf57de3e9065127349495f5e9a9b74d165d005
Shantnau Mandal <shantanumandal.dev@gmail.com>
2025-11-16 23:19:49 +0530

    Profile Done

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,76 @@
+import {
+	BadgeInfo,
+	BadgePlus,
+	CircleUserRound,
+	LogIn,
+	UserRoundCheck,
+} from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
-import ThemeToggleButton from "../ThemeToggleButton";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "../shadcnui/dropdown-menu";
 
 const Header = () => {
 	return (
 		<header
-			className="fixed right-0 left-0 border-b shadow"
+			className="fixed right-0 left-0"
 			aria-label="app-header">
 			<div className="mx-auto flex max-w-7xl items-center justify-between px-6 py-3">
 				<Link href={"/"}>
-					<h1
-						className="text-2xl font-semibold"
-						aria-label="App Name">
-						NSP App
-					</h1>
+					<div className="flex items-center gap-4">
+						<Image
+							src="/shoplane logo.png"
+							alt="image"
+							height={40}
+							width={40}
+						/>
+						<h1
+							className="text-2xl font-extralight"
+							aria-label="App Name">
+							SHOPLANE
+						</h1>
+					</div>
 				</Link>
 
 				<nav className="flex items-center gap-4">
-					<Link href={"/"}>Home</Link>
+					<Link href={"/"}></Link>
+					<Link href={"/"}>
+						<DropdownMenu>
+							<DropdownMenuTrigger className="flex cursor-pointer gap-2">
+								<CircleUserRound />
+								Profile
+							</DropdownMenuTrigger>
+							<DropdownMenuContent>
+								<DropdownMenuLabel>
+									Welcome <br />{" "}
+									<p className="font-light">
+										To access account and manage orders
+									</p>
+								</DropdownMenuLabel>
+								<DropdownMenuSeparator />
+								<DropdownMenuItem>
+									Log In <LogIn />
+								</DropdownMenuItem>
+								<DropdownMenuItem>
+									Sign Up <UserRoundCheck />
+								</DropdownMenuItem>
+								<DropdownMenuItem>
+									Shoplane Premium <BadgePlus color="#ff0000" />
+								</DropdownMenuItem>
+								<DropdownMenuItem>
+									Help & Support <BadgeInfo />
+								</DropdownMenuItem>
+							</DropdownMenuContent>
+						</DropdownMenu>
+					</Link>
 
-					<ThemeToggleButton />
+					{/* <ThemeToggleButton /> */}
 				</nav>
 			</div>
 		</header>
